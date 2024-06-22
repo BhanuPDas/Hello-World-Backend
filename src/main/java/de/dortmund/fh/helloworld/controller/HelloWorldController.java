e631d2bb3351a0b903e57c25695842029633c152
BhanuPDas <bhanu.das001@stud.fh-dortmund.de>
2024-06-22 19:18:08 +0200

    Added changes

--- src/main/java/de/dortmund/fh/helloworld/controller/HelloWorldController.java
+++ src/main/java/de/dortmund/fh/helloworld/controller/HelloWorldController.java
@@ -3,12 +3,14 @@ package de.dortmund.fh.helloworld.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(allowedHeaders = "*")
 public class HelloWorldController {
 
 	@Value("${color}")
